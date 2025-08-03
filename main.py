b26c050fbe3e8c7643844ed230c383f26282c71c
snrredoy <snrredoy3@gmail.com>
2025-08-03 17:13:26 +0600

    practice with path parameter.

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {'message': 'Hello FastAPI.'}
+
+
+@app.get('/item/{item_id}')
+async def read_item(item_id: int):
+    return {'item id': item_id}
+
+
+@app.get('/user/me')
+async def read_current_user():
+    return {'user_id': 'the current user.'}
+
+
+@app.get('/user/{user_id}')
+async def read_user(user_id: int):
+    return {'user_id': user_id}
