63b1efbd295e0b6b5527f8b0933862ea67150cdf
snrredoy <snrredoy3@gmail.com>
2025-08-05 09:53:29 +0600

    Add item and user retrieval endpoints with optional query parameters

--- main.py
+++ main.py
@@ -20,3 +20,36 @@ async def read_current_user():
 @app.get('/user/{user_id}')
 async def read_user(user_id: int):
     return {'user_id': user_id}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get('/items/')
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get('/items/{item_id}')
+async def read_items(item_id: int , q: str | None = None):
+    if q:
+        return {'item_id': item_id, 'q': q}
+    return {'item_id': item_id}
+
+
+@app.get('/optional/{item_id}')
+async def read_items(item_id: int , q: str | None = None, short: bool = False):
+    item = {'item_id': item_id}
+    if q:
+        item.update({'q': q})
+    if not short:
+        item.update({'description': 'This is an amezing item that has a long description.'})
+    return item
+
+
+@app.get('/item/{item_id}/user/{user_id}')
+async def read_items(item_id: int , user_id: int, q: str | None = None, short: bool = False):
+    item = {'item_id': item_id, 'user_id': user_id}
+    if q:
+        item.update({'q': q})
+    if not short:
+        item.update({'description': 'This is an amezing item that has a long description.'})
+    return item
