ec0b5867839cc5024af03383cecaff5a00ca6f44
snrredoy <snrredoy3@gmail.com>
2025-08-05 15:57:39 +0600

    Add product creation and update endpoints with Pydantic model validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -63,3 +64,30 @@ async def read_items(item_id: int , user_id: int, needy: str, q: str | None = No
     if not short:
         item.update({'description': 'This is an amezing item that has a long description.'})
     return item
+
+
+class Product(BaseModel):
+    name : str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post('/product/')
+async def create_product(item: Product):
+    item_dict = item.dict()
+    if item.tax is not None: 
+        price_with_tax = item.price + item.tax
+        item_dict.update({'price_with_tax': price_with_tax})
+    return item_dict
+
+
+@app.put('/product/{product_id}')
+async def update_product(product_id: int, product: Product, q: str | None = None):
+    return {
+        'product_id': product_id,
+        **product.dict(),
+        'q': q
+    }
+
+
