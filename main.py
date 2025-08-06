6c3629078e314b1cec0c8794595f34e180344aea
snrredoy <snrredoy3@gmail.com>
2025-08-06 15:45:09 +0600

    Add search endpoint with ID validation and random item retrieval

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Query
-from pydantic import BaseModel
+from pydantic import BaseModel, AfterValidator
 from typing import Annotated
+import random
+
 
 app = FastAPI()
 
@@ -169,3 +171,24 @@ async def read_card(q: Annotated[str | None, Query(alias='item-query', deprecate
     if q:
         results.update({"q": q})
     return results
+
+
+data = {
+    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
+    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
+    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
+}
+
+def check_valid_id(id: str):
+    if not id.startswith(('isbn-', 'imdb-')):
+        raise ValueError('Invalid ID format, it must start with "isbn-" or "imdb-"')
+    return id
+
+
+@app.get('/search/')
+async def read_val(id: Annotated[str | None, AfterValidator(check_valid_id)] = None):
+    if id:
+        item = data.get(id)
+    else:
+        id, item = random.choice(list(data.items()))
+    return {'id': id, 'name': item}
