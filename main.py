767135ab025b00d2a15ff9332887ac9dc770469d
snrredoy <snrredoy3@gmail.com>
2025-08-19 08:07:26 +0600

    Add endpoints for item retrieval with detailed response models

--- main.py
+++ main.py
@@ -460,3 +460,35 @@ async def read_portal(teleport: bool = False) -> Response | dict:
     if teleport:
         return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     return JSONResponse(content={"message": "Here's your interdimensional portal."})
+
+
+class Item4(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float = 10.5
+    tags: list[str] = []
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+@app.get('/item4/{item_id}', response_model=Item4, response_model_exclude_unset=True)
+async def read_item(item_id: str):
+    return items[item_id]
+
+
+@app.get(
+    "/item4/{item_id}/name",
+    response_model=Item4,
+    response_model_include={"name", "description"},
+)
+async def read_item_name(item_id: str):
+    return items[item_id]
+
+
+@app.get("/item4/{item_id}/public", response_model=Item4, response_model_exclude={"tax"})
+async def read_item_public_data(item_id: str):
+    return items[item_id]
