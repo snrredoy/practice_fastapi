8936e3df26551be986e548a5c26dbaab1333129f
snrredoy <snrredoy3@gmail.com>
2025-08-23 19:16:22 +0600

    Add header handling and error response for item retrieval

--- main.py
+++ main.py
@@ -599,3 +599,13 @@ async def read_item(item_id: str):
     return {
         'item': items[item_id]
     }
+
+items = {"foo": "The Foo Wrestlers"}
+async def read_item_header(item_id: str):
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error": "There goes my error"},
+        )
+    return {"item": items[item_id]}
