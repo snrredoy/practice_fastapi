e92f39007b68ac85c6d9f322fef8641ec88b5b1f
snrredoy <snrredoy3@gmail.com>
2025-08-24 17:22:06 +0600

    Add custom exception handlers for HTTP and validation errors, and implement a new endpoint for item retrieval

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from typing import Annotated, Literal, Any, Union
 import random
 from datetime import datetime, timedelta, time
 from uuid import UUID
-from fastapi.responses import JSONResponse, RedirectResponse, HTMLResponse
+from fastapi.responses import JSONResponse, RedirectResponse, HTMLResponse, PlainTextResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
@@ -630,3 +632,20 @@ async def read_unicorn(name: str):
     if name == "yolo":
         raise UnicornException(name=name)
     return {"unicorn_name": name}
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return PlainTextResponse(str(exc), status_code=400)
+
+
+@app.get("/itemss/{item_id}")
+async def read_item(item_id: int):
+    if item_id == 3:
+        raise HTTPException(status_code=418, detail="Nope! I don't like 3.")
+    return {"item_id": item_id}
