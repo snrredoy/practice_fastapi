61b8532242c638c18a5f8a15b6d55f6e7a541fc9
snrredoy <snrredoy3@gmail.com>
2025-08-24 16:27:45 +0600

    Add custom exception handling and new endpoints for item headers and unicorns

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Path, Body, Cookie, Response, Header, Form, File, UploadFile, HTTPException
+from fastapi import FastAPI, Query, Path, Body, Cookie, Response, Header, Form, File, UploadFile, HTTPException,Request
 from pydantic import BaseModel, AfterValidator, Field, HttpUrl, EmailStr
 from typing import Annotated, Literal, Any, Union
 import random
@@ -601,6 +601,7 @@ async def read_item(item_id: str):
     }
 
 items = {"foo": "The Foo Wrestlers"}
+@app.get("/items-header/{item_id}")
 async def read_item_header(item_id: str):
     if item_id not in items:
         raise HTTPException(
@@ -609,3 +610,23 @@ async def read_item_header(item_id: str):
             headers={"X-Error": "There goes my error"},
         )
     return {"item": items[item_id]}
+
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
+
+
+@app.get("/unicorns/{name}")
+async def read_unicorn(name: str):
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
