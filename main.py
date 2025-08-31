3a75c8fc3b261a00c35472f2616cdd3dc6715723
snrredoy <snrredoy3@gmail.com>
2025-08-31 16:03:01 +0600

    Add dependency injection examples with dummy classes and functions

--- main.py
+++ main.py
@@ -778,3 +778,50 @@ async def read_items():
 @app.get("/global-users/")
 async def read_users():
     return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+
+# Dummy classes and functions for dependency example
+class DepA:
+    def close(self):
+        pass
+
+class DepB:
+    def close(self, dep_a):
+        pass
+
+class DepC:
+    def close(self, dep_b):
+        pass
+
+def generate_dep_a():
+    return DepA()
+
+def generate_dep_b():
+    return DepB()
+
+def generate_dep_c():
+    return DepC()
+
+async def dependency_a():
+    dep_a = generate_dep_a()
+    try:
+        yield dep_a
+    finally:
+        dep_a.close()
+
+
+async def dependency_b(dep_a: Annotated[DepA, Depends(dependency_a)]):
+    dep_b = generate_dep_b()
+    try:
+        yield dep_b
+    finally:
+        dep_b.close(dep_a)
+
+
+async def dependency_c(dep_b: Annotated[DepB, Depends(dependency_b)]):
+    dep_c = generate_dep_c()
+    try:
+        yield dep_c
+    finally:
+        dep_c.close(dep_b)
