d6cb53898d715995331780aca6c954f8c515d066
snrredoy <snrredoy3@gmail.com>
2025-09-03 12:53:33 +0600

    Add SQLModel and SQLAlchemy dependencies; define Hero model and initialize SQLite database

--- main.py
+++ main.py
@@ -1119,3 +1119,17 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+from sqlmodel import SQLModel, Field, create_engine
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(primary_key=True, default=None)
+    name: str
+    secret_name: str 
+    age: int | None = None
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+SQLModel.metadata.create_all(engine)
