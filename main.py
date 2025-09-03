e4f8ebfb96ffc1fbf06d3af3d8792a32eedc50f7
snrredoy <snrredoy3@gmail.com>
2025-09-03 14:16:20 +0600

    Add database setup and hero creation functions; initialize SQLite database with sample data

--- main.py
+++ main.py
@@ -1120,7 +1120,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-from sqlmodel import SQLModel, Field, create_engine
+from sqlmodel import SQLModel, Field, create_engine, Session
 
 class Hero(SQLModel, table=True):
     id: int | None = Field(primary_key=True, default=None)
@@ -1132,4 +1132,28 @@ sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=True)
-SQLModel.metadata.create_all(engine)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def create_heroes():  
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")  
+    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
+
+    with Session(engine) as session:  
+        session.add(hero_1)  
+        session.add(hero_2)
+        session.add(hero_3)
+
+        session.commit()  
+    
+
+
+def main():  
+    create_db_and_tables()  
+    create_heroes()  
+
+
+if __name__ == "__main__":  
+    main()  
