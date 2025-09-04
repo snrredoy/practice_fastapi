0b3b44d469fe2cbbf596026f48a6f3d10d9a5b62
snrredoy <snrredoy3@gmail.com>
2025-09-04 09:18:21 +0600

    Enhance FastAPI app initialization; add detailed API description and metadata for users and items

--- main.py
+++ main.py
@@ -11,7 +11,61 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+description = """
+ChimichangApp API helps you do awesome stuff. 🚀
+
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+
+app = FastAPI(
+    title="ChimichangApp",
+    description=description,
+    summary="Deadpool's favorite app. Nuff said.",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    # license_info={
+    #     "name": "Apache 2.0",
+    #     "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    # },
+    license_info={
+        "name": "Apache 2.0",
+        "identifier": "MIT",
+    },
+    openapi_tags= tags_metadata,
+    openapi_url="/api/v1/openapi.json",
+    docs_url="/documentation",
+    redoc_url=None
+)
 
 @app.get('/')
 async def root():
